03b7cf64510993ebf6315f784aa0bbb4b9b8a674
MegaEmme <mikmusco@gmail.com>
2025-02-27 13:06:13 +0100

    feat: improved es.3

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -1,13 +1,14 @@
-console.log("Mike's jsnacks")
+console.log("Mike's jsnacks");
 
 // jsnack3
 // Il software deve chiedere per 10 volte all’utente di inserire un numero.
 // Il programma stampa la somma di tutti i numeri inseriti.
 
-let sum = 0
+let sum = 0;
 
-for (let i=0; i<=9; i++){
+for (let i=0; i<10; i++){
     let newNumbers = Number(prompt('inserisci numero'));
-    sum += newNumbers  
+    sum += newNumbers;
 }
-console.log(`la somma totale è ${sum}`)
+
+console.log(`la somma totale è ${sum}`);
