bf6a79c4a3fb0fc2ed8a2b62a3d5b6b13c3eb696
MegaEmme <mikmusco@gmail.com>
2025-02-27 12:25:30 +0100

    solved es.2

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -1,1 +1,14 @@
 console.log("Mike's jsnacks")
+
+// jsnack2
+// L’utente inserisce due parole in successione, con due prompt.
+// Il software stampa prima la parola più corta, poi la parola più lunga.
+
+const wordOne = prompt('Inserisci parola uno');
+const wordTwo = prompt('Inserisci parola due');
+
+if (wordOne.length > wordTwo.length){
+    console.log(`la parola più lunga è ${wordOne}`);
+} else {
+    console.log(`la parola più lunga è ${wordTwo}`);
+}
