75951904b1782a72ae952b5e5528e8c199e060f0
MegaEmme <mikmusco@gmail.com>
2025-02-27 12:20:17 +0100

    solved es.1

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,1 +1,13 @@
 console.log("Mike's jsnacks")
+
+// jsnack1
+// L’utente inserisce due numeri in successione, con due prompt.
+// Il software stampa il maggiore.
+
+const numOne = prompt('Inserisci il primo numero');
+const numTwo = prompt('inserisci il secondo numero');
+ if (numOne > numTwo){
+    console.log(`${numOne} è il maggiore`);
+ } else {
+    console.log(`${numTwo20} è il maggiore`);
+ }
