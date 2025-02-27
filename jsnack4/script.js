1ea5f678a096d949c17a4ce50f600b98f90507c9
MegaEmme <mikmusco@gmail.com>
2025-02-27 12:43:15 +0100

    solved es.4

--- jsnack4/script.js
+++ jsnack4/script.js
@@ -1,1 +1,17 @@
 console.log("Mike's jsnacks")
+
+// jsnack4
+// Crea un array vuoto.
+// Chiedi per 6 volte all’utente di inserire un numero,
+// se è dispari inseriscilo nell’array.
+
+const number = []
+
+for (i=0; i<5; i++){
+    let newNumber = prompt('inserisci numero')
+    if (newNumber%2 !==0){
+        number.push(newNumber)
+    }
+}
+
+ console.log(number)
