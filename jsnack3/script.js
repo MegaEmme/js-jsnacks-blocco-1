b49e5f87a0b3a04029b808088d513e4328ca495e
MegaEmme <mikmusco@gmail.com>
2025-02-27 12:44:34 +0100

    started es.3

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -1,1 +1,13 @@
 console.log("Mike's jsnacks")
+
+// jsnack3
+// Il software deve chiedere per 10 volte all’utente di inserire un numero.
+// Il programma stampa la somma di tutti i numeri inseriti.
+
+
+for (let i=0; i<=9; i++){
+    let newNumber = prompt('inserisci numero');
+    if (newNumber%2 !== 0){
+        
+    }   
+}
