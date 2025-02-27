c4b0aa16d1f63fde4a1ff788fd65c97a6ac92c85
MegaEmme <mikmusco@gmail.com>
2025-02-27 13:03:46 +0100

    feat: improved es.2

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -5,10 +5,13 @@ console.log("Mike's jsnacks")
 // Il software stampa prima la parola più corta, poi la parola più lunga.
 
 const wordOne = prompt('Inserisci parola uno');
+;
 const wordTwo = prompt('Inserisci parola due');
 
-if (wordOne.length > wordTwo.length){
+if (wordOne.length > wordTwo.length) {
     console.log(`la parola più lunga è ${wordOne}`);
-} else {
+} else if (wordOne.length < wordTwo.length){
     console.log(`la parola più lunga è ${wordTwo}`);
+} else {
+    console.log(`le parole sono lunghe uguale`);
 }
